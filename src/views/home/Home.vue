addfa94b6d1f45f29c8c030ca5406fd8c67c03c1
hzt <1473337046@qq.com>
2021-09-05 15:47:13 +0800

    recommend

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,27 +2,25 @@
   <div id="home">
     <!-- 在组件里面填插槽 -->
     <nav-bar class="home-nav"><template v-slot:center>购物街</template></nav-bar>
-    <swiper>
-      <swiper-item v-for="(item,index) in banners.list" :key="index">
-        <a :href="item.link">
-        <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend :recommends="recommends"></home-recommend>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommend from './childComps/HomeRecommend'
 import {getHomeMultidata} from 'network/home'
-import {Swiper,SwiperItem} from 'components/common/swiper'
+
 
 export default{
   name:"Home",
   components:{
     NavBar,
-    Swiper,
-    SwiperItem
+    HomeSwiper,
+    HomeRecommend
+    
    },
 
   //将请求到的数据保存起来，因为getHomeMultidata函数调用完毕后里面数据会回收
@@ -34,17 +32,16 @@ export default{
 
     }
   },
-
    //组件创建好就发送网络请求，创建生命周期函数
    created() {
      //1.请求多个数据
      getHomeMultidata().then(res=>{
        //res就是请求到的数据
-      //console.log(res);
+      console.log(res);
       //this.result= res;
-      this.banners=res.data.banner;
-      this.recommends=res.data.recommend;
-      console.log(this.banners.list[0].link);
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list;
+      //console.log(this.banners);
      }) 
        }
 
---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="recommend">
+   <div  class="recommend-item" v-for="(item,index) in recommends" :key="index">
+    <a :href="item.link">
+      <img :src="item.image" alt="">
+      <div>{{item.title}}</div>
+    </a>
+   </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'HomeRecommend',
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components:{
+   },
+  data(){
+    return{
+    }
+  }
+}
+</script>
+
+<style scoped>
+.recommend{
+  display:flex;
+  width:100%;
+  text-align: center;
+  font-size: 13px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: solid 10px #eee; 
+}
+.recommend-item{
+  flex:1;
+  
+}
+.recommend-item img{
+   height:65px;
+   margin-bottom:10px;
+}
+
+</style>
